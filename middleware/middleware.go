f4c1fed873e8999177552d72980ded38714555da
Steven Edwards <steven@stephenwithav.com>
2022-10-17 13:28:26 -0400

    Fix ProtectMiddleware.

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -4,13 +4,13 @@ import "net/http"
 
 // ProtectMiddleware
 func ProtectMiddleware(next http.Handler) http.HandlerFunc {
-	defer func() {
-		if err := recover(); err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-		}
-	}()
-
 	return func(w http.ResponseWriter, r *http.Request) {
+		defer func() {
+			if err := recover(); err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+			}
+		}()
+
 		next.ServeHTTP(w, r)
 	}
 }
